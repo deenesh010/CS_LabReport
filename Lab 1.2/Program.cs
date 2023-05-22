bdf9e95f17bc8452f6ea084e6295f5ed1e54cc9c
deenesh010 <dinesh.shrestha2001@gmail.com>
2023-05-22 19:51:17 +0545

    sc

---
+++ Lab 1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2;
+            Console.WriteLine("Enter two numbers: ");
+            Num1 = Convert.ToInt16(Console.ReadLine());
+            Num2 = Convert.ToInt16(Console.ReadLine());
+            int sum=Num1 + Num2;
+            Console.WriteLine("Sum of two numbers is : " + sum);
+            Console.ReadLine();
+        }
+    }
+}
